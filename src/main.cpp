1a4b11730199b8cff547bcfa1659de85fb5f4045
fooooooop <dmurara2@gmail.com>
2025-03-11 08:38:09 +1300

    Updated ReadMe

--- src/main.cpp
+++ src/main.cpp
@@ -10,73 +10,74 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-}/*
-  MechEng 706 Base Code
+} /*
+   MechEng 706 Base Code
 
-  This code provides basic movement and sensor reading for the MechEng 706 Mecanum Wheel Robot Project
+   This code provides basic movement and sensor reading for the MechEng 706
+   Mecanum Wheel Robot Project
 
-  Hardware:
-    Arduino Mega2560 https://www.arduino.cc/en/Guide/ArduinoMega2560
-    MPU-9250 https://www.sparkfun.com/products/13762
-    Ultrasonic Sensor - HC-SR04 https://www.sparkfun.com/products/13959
-    Infrared Proximity Sensor - Sharp https://www.sparkfun.com/products/242
-    Infrared Proximity Sensor Short Range - Sharp https://www.sparkfun.com/products/12728
-    Servo - Generic (Sub-Micro Size) https://www.sparkfun.com/products/9065
-    Vex Motor Controller 29 https://www.vexrobotics.com/276-2193.html
-    Vex Motors https://www.vexrobotics.com/motors.html
-    Turnigy nano-tech 2200mah 2S https://hobbyking.com/en_us/turnigy-nano-tech-2200mah-2s-25-50c-lipo-pack.html
+   Hardware:
+     Arduino Mega2560 https://www.arduino.cc/en/Guide/ArduinoMega2560
+     MPU-9250 https://www.sparkfun.com/products/13762
+     Ultrasonic Sensor - HC-SR04 https://www.sparkfun.com/products/13959
+     Infrared Proximity Sensor - Sharp https://www.sparkfun.com/products/242
+     Infrared Proximity Sensor Short Range - Sharp
+   https://www.sparkfun.com/products/12728 Servo - Generic (Sub-Micro Size)
+   https://www.sparkfun.com/products/9065 Vex Motor Controller 29
+   https://www.vexrobotics.com/276-2193.html Vex Motors
+   https://www.vexrobotics.com/motors.html Turnigy nano-tech 2200mah 2S
+   https://hobbyking.com/en_us/turnigy-nano-tech-2200mah-2s-25-50c-lipo-pack.html
 
-  Date: 11/11/2016
-  Author: Logan Stuart
-  Modified: 15/02/2018
-  Author: Logan Stuart
-*/
+   Date: 11/11/2016
+   Author: Logan Stuart
+   Modified: 15/02/2018
+   Author: Logan Stuart
+ */
 #include <Servo.h>  //Need for Servo pulse output
 
-//#define NO_READ_GYRO  //Uncomment if GYRO is not attached.
-//#define NO_HC-SR04 //Uncomment if HC-SR04 ultrasonic ranging sensor is not attached.
-//#define NO_BATTERY_V_OK //Uncomment if BATTERY_V_OK if you do not care about battery damage.
+// #define NO_READ_GYRO  //Uncomment if GYRO is not attached.
+// #define NO_HC-SR04 //Uncomment if HC-SR04 ultrasonic ranging sensor is not
+// attached. #define NO_BATTERY_V_OK //Uncomment if BATTERY_V_OK if you do not
+// care about battery damage.
 
-//State machine states
-enum STATE {
-  INITIALISING,
-  RUNNING,
-  STOPPED
-};
+// State machine states
+enum STATE { INITIALISING, RUNNING, STOPPED };
 
-//Refer to Shield Pinouts.jpg for pin locations
+// Refer to Shield Pinouts.jpg for pin locations
 
-//Default motor control pins
+// Default motor control pins
 const byte left_front = 46;
 const byte left_rear = 47;
 const byte right_rear = 50;
 const byte right_front = 51;
 
-
-//Default ultrasonic ranging sensor pins, these pins are defined my the Shield
+// Default ultrasonic ranging sensor pins, these pins are defined my the Shield
 const int TRIG_PIN = 48;
 const int ECHO_PIN = 49;
 
-// Anything over 400 cm (23200 us pulse) is "out of range". Hit:If you decrease to this the ranging sensor but the timeout is short, you may not need to read up to 4meters.
+// Anything over 400 cm (23200 us pulse) is "out of range". Hit:If you decrease
+// to this the ranging sensor but the timeout is short, you may not need to read
+// up to 4meters.
 const unsigned int MAX_DIST = 23200;
 
-Servo left_font_motor;  // create servo object to control Vex Motor Controller 29
-Servo left_rear_motor;  // create servo object to control Vex Motor Controller 29
-Servo right_rear_motor;  // create servo object to control Vex Motor Controller 29
-Servo right_font_motor;  // create servo object to control Vex Motor Controller 29
+Servo
+    left_font_motor;  // create servo object to control Vex Motor Controller 29
+Servo
+    left_rear_motor;  // create servo object to control Vex Motor Controller 29
+Servo
+    right_rear_motor;  // create servo object to control Vex Motor Controller 29
+Servo
+    right_font_motor;  // create servo object to control Vex Motor Controller 29
 Servo turret_motor;
-
 
 int speed_val = 100;
 int speed_change;
 
-//Serial Pointer
+// Serial Pointer
 HardwareSerial *SerialCom;
 
-
 int pos = 0;
-void setup(void)
-{
+void setup(void) {
   turret_motor.attach(11);
   pinMode(LED_BUILTIN, OUTPUT);
 
@@ -84,39 +85,39 @@ void setup(void)
   pinMode(TRIG_PIN, OUTPUT);
   digitalWrite(TRIG_PIN, LOW);
 
-  // Setup the Serial port and pointer, the pointer allows switching the debug info through the USB port(Serial) or Bluetooth port(Serial1) with ease.
+  // Setup the Serial port and pointer, the pointer allows switching the debug
+  // info through the USB port(Serial) or Bluetooth port(Serial1) with ease.
   SerialCom = &Serial;
   SerialCom->begin(115200);
   SerialCom->println("MECHENG706_Base_Code_25/01/2018");
   delay(1000);
   SerialCom->println("Setup....");
 
-  delay(1000); //settling time but no really needed
-
+  delay(1000);  // settling time but no really needed
 }
 
-void loop(void) //main loop
+void loop(void)  // main loop
 {
   static STATE machine_state = INITIALISING;
-  //Finite-state machine Code
+  // Finite-state machine Code
   switch (machine_state) {
     case INITIALISING:
       machine_state = initialising();
       break;
-    case RUNNING: //Lipo Battery Volage OK
-      machine_state =  running();
+    case RUNNING:  // Lipo Battery Volage OK
+      machine_state = running();
       break;
-    case STOPPED: //Stop of Lipo Battery voltage is too low, to protect Battery
-      machine_state =  stopped();
+    case STOPPED:  // Stop of Lipo Battery voltage is too low, to protect
+                   // Battery
+      machine_state = stopped();
       break;
   };
 }
 
-
 STATE initialising() {
-  //initialising
+  // initialising
   SerialCom->println("INITIALISING....");
-  delay(1000); //One second delay to see the serial string "INITIALISING...."
+  delay(1000);  // One second delay to see the serial string "INITIALISING...."
   SerialCom->println("Enabling Motors...");
   enable_motors();
   SerialCom->println("RUNNING STATE...");
@@ -124,13 +125,13 @@ STATE initialising() {
 }
 
 STATE running() {
-
   static unsigned long previous_millis;
 
   read_serial_command();
   fast_flash_double_LED_builtin();
 
-  if (millis() - previous_millis > 500) {  //Arduino style 500ms timed execution statement
+  if (millis() - previous_millis >
+      500) {  // Arduino style 500ms timed execution statement
     previous_millis = millis();
 
     SerialCom->println("RUNNING---------");
@@ -141,7 +142,7 @@ STATE running() {
     GYRO_reading();
 #endif
 
-#ifndef NO_HC-SR04
+#ifndef NO_HC - SR04
     HC_SR04_range();
 #endif
 
@@ -149,15 +150,11 @@ STATE running() {
     if (!is_battery_voltage_OK()) return STOPPED;
 #endif
 
-
     turret_motor.write(pos);
 
-    if (pos == 0)
-    {
+    if (pos == 0) {
       pos = 45;
-    }
-    else
-    {
+    } else {
       pos = 0;
     }
   }
@@ -165,7 +162,7 @@ STATE running() {
   return RUNNING;
 }
 
-//Stop of Lipo Battery voltage is too low, to protect Battery
+// Stop of Lipo Battery voltage is too low, to protect Battery
 STATE stopped() {
   static byte counter_lipo_voltage_ok;
   static unsigned long previous_millis;
@@ -173,25 +170,23 @@ STATE stopped() {
   disable_motors();
   slow_flash_LED_builtin();
 
-  if (millis() - previous_millis > 500) { //print massage every 500ms
+  if (millis() - previous_millis > 500) {  // print massage every 500ms
     previous_millis = millis();
     SerialCom->println("STOPPED---------");
 
-
 #ifndef NO_BATTERY_V_OK
-    //500ms timed if statement to check lipo and output speed settings
+    // 500ms timed if statement to check lipo and output speed settings
     if (is_battery_voltage_OK()) {
       SerialCom->print("Lipo OK waiting of voltage Counter 10 < ");
       SerialCom->println(counter_lipo_voltage_ok);
       counter_lipo_voltage_ok++;
-      if (counter_lipo_voltage_ok > 10) { //Making sure lipo voltage is stable
+      if (counter_lipo_voltage_ok > 10) {  // Making sure lipo voltage is stable
         counter_lipo_voltage_ok = 0;
         enable_motors();
         SerialCom->println("Lipo OK returning to RUN STATE");
         return RUNNING;
       }
-    } else
-    {
+    } else {
       counter_lipo_voltage_ok = 0;
     }
 #endif
@@ -199,8 +194,7 @@ STATE stopped() {
   return STOPPED;
 }
 
-void fast_flash_double_LED_builtin()
-{
+void fast_flash_double_LED_builtin() {
   static byte indexer = 0;
   static unsigned long fast_flash_millis;
   if (millis() > fast_flash_millis) {
@@ -216,8 +210,7 @@ void fast_flash_double_LED_builtin()
   }
 }
 
-void slow_flash_LED_builtin()
-{
+void slow_flash_LED_builtin() {
   static unsigned long slow_flash_millis;
   if (millis() - slow_flash_millis > 2000) {
     slow_flash_millis = millis();
@@ -225,25 +218,24 @@ void slow_flash_LED_builtin()
   }
 }
 
-void speed_change_smooth()
-{
+void speed_change_smooth() {
   speed_val += speed_change;
-  if (speed_val > 1000)
-    speed_val = 1000;
+  if (speed_val > 1000) speed_val = 1000;
   speed_change = 0;
 }
 
 #ifndef NO_BATTERY_V_OK
-boolean is_battery_voltage_OK()
-{
+boolean is_battery_voltage_OK() {
   static byte Low_voltage_counter;
   static unsigned long previous_millis;
 
   int Lipo_level_cal;
   int raw_lipo;
-  //the voltage of a LiPo cell depends on its chemistry and varies from about 3.5V (discharged) = 717(3.5V Min) https://oscarliang.com/lipo-battery-guide/
-  //to about 4.20-4.25V (fully charged) = 860(4.2V Max)
-  //Lipo Cell voltage should never go below 3V, So 3.5V is a safety factor.
+  // the voltage of a LiPo cell depends on its chemistry and varies from
+  // about 3.5V (discharged) = 717(3.5V Min)
+  // https://oscarliang.com/lipo-battery-guide/ to about 4.20-4.25V (fully
+  // charged) = 860(4.2V Max) Lipo Cell voltage should never go below 3V,
+  // So 3.5V is a safety factor.
   raw_lipo = analogRead(A0);
   Lipo_level_cal = (raw_lipo - 717);
   Lipo_level_cal = Lipo_level_cal * 100;
@@ -261,11 +253,13 @@ boolean is_battery_voltage_OK()
     return true;
   } else {
     if (Lipo_level_cal < 0)
-      SerialCom->println("Lipo is Disconnected or Power Switch is turned OFF!!!");
+      SerialCom->println(
+          "Lipo is Disconnected or Power Switch is turned OFF!!!");
     else if (Lipo_level_cal > 160)
       SerialCom->println("!Lipo is Overchanged!!!");
     else {
-      SerialCom->println("Lipo voltage too LOW, any lower and the lipo with be damaged");
+      SerialCom->println(
+          "Lipo voltage too LOW, any lower and the lipo with be damaged");
       SerialCom->print("Please Re-charge Lipo:");
       SerialCom->print(Lipo_level_cal);
       SerialCom->println("%");
@@ -277,13 +271,11 @@ boolean is_battery_voltage_OK()
     else
       return true;
   }
-
 }
 #endif
 
-#ifndef NO_HC-SR04
-void HC_SR04_range()
-{
+#ifndef NO_HC - SR04
+void HC_SR04_range() {
   unsigned long t1;
   unsigned long t2;
   unsigned long pulse_width;
@@ -297,10 +289,10 @@ void HC_SR04_range()
 
   // Wait for pulse on echo pin
   t1 = micros();
-  while ( digitalRead(ECHO_PIN) == 0 ) {
+  while (digitalRead(ECHO_PIN) == 0) {
     t2 = micros();
     pulse_width = t2 - t1;
-    if ( pulse_width > (MAX_DIST + 1000)) {
+    if (pulse_width > (MAX_DIST + 1000)) {
       SerialCom->println("HC-SR04: NOT found");
       return;
     }
@@ -310,11 +302,10 @@ void HC_SR04_range()
   // Note: the micros() counter will overflow after ~70 min
 
   t1 = micros();
-  while ( digitalRead(ECHO_PIN) == 1)
-  {
+  while (digitalRead(ECHO_PIN) == 1) {
     t2 = micros();
     pulse_width = t2 - t1;
-    if ( pulse_width > (MAX_DIST + 1000) ) {
+    if (pulse_width > (MAX_DIST + 1000)) {
       SerialCom->println("HC-SR04: Out of range");
       return;
     }
@@ -325,12 +316,12 @@ void HC_SR04_range()
 
   // Calculate distance in centimeters and inches. The constants
   // are found in the datasheet, and calculated from the assumed speed
-  //of sound in air at sea level (~340 m/s).
+  // of sound in air at sea level (~340 m/s).
   cm = pulse_width / 58.0;
   inches = pulse_width / 148.0;
 
   // Print out results
-  if ( pulse_width > MAX_DIST ) {
+  if (pulse_width > MAX_DIST) {
     SerialCom->println("HC-SR04: Out of range");
   } else {
     SerialCom->print("HC-SR04:");
@@ -340,62 +331,59 @@ void HC_SR04_range()
 }
 #endif
 
-void Analog_Range_A4()
-{
+void Analog_Range_A4() {
   SerialCom->print("Analog Range A4:");
   SerialCom->println(analogRead(A4));
 }
 
 #ifndef NO_READ_GYRO
-void GYRO_reading()
-{
+void GYRO_reading() {
   SerialCom->print("GYRO A3:");
   SerialCom->println(analogRead(A3));
 }
 #endif
 
-//Serial command pasing
-void read_serial_command()
-{
+// Serial command pasing
+void read_serial_command() {
   if (SerialCom->available()) {
     char val = SerialCom->read();
     SerialCom->print("Speed:");
     SerialCom->print(speed_val);
     SerialCom->print(" ms ");
 
-    //Perform an action depending on the command
+    // Perform an action depending on the command
     switch (val) {
-      case 'w'://Move Forward
+      case 'w':  // Move Forward
       case 'W':
-        forward ();
+        forward();
         SerialCom->println("Forward");
         break;
-      case 's'://Move Backwards
+      case 's':  // Move Backwards
       case 'S':
-        reverse ();
+        reverse();
         SerialCom->println("Backwards");
         break;
-      case 'q'://Turn Left
+      case 'q':  // Turn Left
       case 'Q':
         strafe_left();
         SerialCom->println("Strafe Left");
         break;
-      case 'e'://Turn Right
+      case 'e':  // Turn Right
       case 'E':
         strafe_right();
         SerialCom->println("Strafe Right");
         break;
-      case 'a'://Turn Right
+      case 'a':  // Turn Right
       case 'A':
         ccw();
         SerialCom->println("ccw");
         break;
-      case 'd'://Turn Right
+      case 'd':  // Turn Right
       case 'D':
         cw();
         SerialCom->println("cw");
         break;
-      case '-'://Turn Right
+      case '-':  // Turn Right
       case '_':
         speed_change = -100;
         SerialCom->println("-100");
@@ -410,20 +398,23 @@ void read_serial_command()
         SerialCom->println("stop");
         break;
     }
-
   }
-
 }
 
 //----------------------Motor moments------------------------
-//The Vex Motor Controller 29 use Servo Control signals to determine speed and direction, with 0 degrees meaning neutral https://en.wikipedia.org/wiki/Servo_control
+// The Vex Motor Controller 29 use Servo Control signals to determine speed and
+// direction, with 0 degrees meaning neutral
+// https://en.wikipedia.org/wiki/Servo_control
 
-void disable_motors()
-{
-  left_font_motor.detach();  // detach the servo on pin left_front to turn Vex Motor Controller 29 Off
-  left_rear_motor.detach();  // detach the servo on pin left_rear to turn Vex Motor Controller 29 Off
-  right_rear_motor.detach();  // detach the servo on pin right_rear to turn Vex Motor Controller 29 Off
-  right_font_motor.detach();  // detach the servo on pin right_front to turn Vex Motor Controller 29 Off
+void disable_motors() {
+  left_font_motor.detach();   // detach the servo on pin left_front to turn Vex
+                              // Motor Controller 29 Off
+  left_rear_motor.detach();   // detach the servo on pin left_rear to turn Vex
+                              // Motor Controller 29 Off
+  right_rear_motor.detach();  // detach the servo on pin right_rear to turn Vex
+                              // Motor Controller 29 Off
+  right_font_motor.detach();  // detach the servo on pin right_front to turn Vex
+                              // Motor Controller 29 Off
 
   pinMode(left_front, INPUT);
   pinMode(left_rear, INPUT);
@@ -431,14 +422,18 @@ void disable_motors()
   pinMode(right_front, INPUT);
 }
 
-void enable_motors()
-{
-  left_font_motor.attach(left_front);  // attaches the servo on pin left_front to turn Vex Motor Controller 29 On
-  left_rear_motor.attach(left_rear);  // attaches the servo on pin left_rear to turn Vex Motor Controller 29 On
-  right_rear_motor.attach(right_rear);  // attaches the servo on pin right_rear to turn Vex Motor Controller 29 On
-  right_font_motor.attach(right_front);  // attaches the servo on pin right_front to turn Vex Motor Controller 29 On
+void enable_motors() {
+  left_font_motor.attach(left_front);  // attaches the servo on pin left_front
+                                       // to turn Vex Motor Controller 29 On
+  left_rear_motor.attach(left_rear);   // attaches the servo on pin left_rear to
+                                       // turn Vex Motor Controller 29 On
+  right_rear_motor.attach(right_rear);  // attaches the servo on pin right_rear
+                                        // to turn Vex Motor Controller 29 On
+  right_font_motor.attach(
+      right_front);  // attaches the servo on pin right_front to turn Vex Motor
+                     // Controller 29 On
 }
-void stop() //Stop
+void stop()  // Stop
 {
   left_font_motor.writeMicroseconds(1500);
   left_rear_motor.writeMicroseconds(1500);
@@ -446,56 +441,47 @@ void stop() //Stop
   right_font_motor.writeMicroseconds(1500);
 }
 
-void forward()
-{
+void forward() {
   left_font_motor.writeMicroseconds(1500 + speed_val);
   left_rear_motor.writeMicroseconds(1500 + speed_val);
   right_rear_motor.writeMicroseconds(1500 - speed_val);
   right_font_motor.writeMicroseconds(1500 - speed_val);
 }
 
-void reverse ()
-{
+void reverse() {
   left_font_motor.writeMicroseconds(1500 - speed_val);
   left_rear_motor.writeMicroseconds(1500 - speed_val);
   right_rear_motor.writeMicroseconds(1500 + speed_val);
   right_font_motor.writeMicroseconds(1500 + speed_val);
 }
 
-void ccw ()
-{
+void ccw() {
   left_font_motor.writeMicroseconds(1500 - speed_val);
   left_rear_motor.writeMicroseconds(1500 - speed_val);
   right_rear_motor.writeMicroseconds(1500 - speed_val);
   right_font_motor.writeMicroseconds(1500 - speed_val);
 }
 
-void cw ()
-{
+void cw() {
   left_font_motor.writeMicroseconds(1500 + speed_val);
   left_rear_motor.writeMicroseconds(1500 + speed_val);
   right_rear_motor.writeMicroseconds(1500 + speed_val);
   right_font_motor.writeMicroseconds(1500 + speed_val);
 }
 
-void strafe_left ()
-{
+void strafe_left() {
   left_font_motor.writeMicroseconds(1500 - speed_val);
   left_rear_motor.writeMicroseconds(1500 + speed_val);
   right_rear_motor.writeMicroseconds(1500 + speed_val);
   right_font_motor.writeMicroseconds(1500 - speed_val);
 }
 
-void strafe_right ()
-{
+void strafe_right() {
   left_font_motor.writeMicroseconds(1500 + speed_val);
   left_rear_motor.writeMicroseconds(1500 - speed_val);
   right_rear_motor.writeMicroseconds(1500 - speed_val);
   right_font_motor.writeMicroseconds(1500 + speed_val);
 }
-
 
 // put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+int myFunction(int x, int y) { return x + y; }
