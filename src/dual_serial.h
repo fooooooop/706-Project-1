92203160d859c220dc9dbf37cf03991b48307733
fooooooop <dmurara2@gmail.com>
2025-04-02 15:08:53 +1300

    Turned off serial in dual_serial.h

--- src/dual_serial.h
+++ src/dual_serial.h
@@ -6,21 +6,21 @@
 // Generic print
 template <typename T>
 void dualPrint(const T& value) {
-  Serial.print(value);
-  Serial1.print(value);
+  // Serial.print(value);
+  // Serial1.print(value);
 }
 
 // Generic println
 template <typename T>
 void dualPrintln(const T& value) {
-  Serial.println(value);
-  Serial1.println(value);
+  // Serial.println(value);
+  // Serial1.println(value);
 }
 
 // Optional: println() with no arguments
 inline void dualPrintln() {
-  Serial.println();
-  Serial1.println();
+  // Serial.println();
+  // Serial1.println();
 }
 
 #endif
